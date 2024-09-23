5a21e6904f580a7446745001e2a37ad3d0a59218
Ayush200704 <ayush.tiwari.200704@gmail.com>
2024-09-24 02:01:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex">
+      <Section title="Page One" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti iste exercitationem nam autem, aperiam corporis qui necessitatibus. Ratione, laudantium vitae. Dolorem, libero quo ratione voluptatem nobis sit nostrum recusandae temporibus?" bgColor="bg-gray-900" />
+      <Section title="Page Two" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti iste exercitationem nam autem, aperiam corporis qui necessitatibus. Ratione, laudantium vitae. Dolorem, libero quo ratione voluptatem nobis sit nostrum recusandae temporibus?" bgColor="bg-black" />
+      <Section title="Page Three" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti iste exercitationem nam autem, aperiam corporis qui necessitatibus. Ratione, laudantium vitae. Dolorem, libero quo ratione voluptatem nobis sit nostrum recusandae temporibus?" bgColor="bg-gray-900" />
+    </div>
+  );
+}
+
+function Section({ title, content, bgColor }) {
+  return (
+    <section className={`flex-none flex-col items-center justify-center h-screen w-full ${bgColor} text-white`}>
+      <h1 className="text-5xl">{title}</h1>
+      <p className="mt-8 leading-8 w-4/5 text-justify">{content}</p>
+    </section>
+  );
+}
+
+export default App;
